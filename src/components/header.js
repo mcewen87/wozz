a48ed632e56f4c62268fc0db6428536386bb9243
Jon McEwen <>
2019-07-01 23:45:51 -0500

    nav bar

--- src/components/header.js
+++ src/components/header.js
@@ -1,55 +1,86 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
-import header from "./header.module.scss"
+// import { Link } from "gatsby"
+// import PropTypes from "prop-types"
+// import React from "react"
+// import header from "./header.module.scss"
 
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+// import { faChevronDown } from "@fortawesome/free-solid-svg-icons"
+
+// const Header = () => (
+//   <header>
+//     <div className={header.navBar}>
+//       <ul>
+//         <li>
+//           <Link className={header.linkFirst} to="/">
+//             Wozz
+//             <FontAwesomeIcon className={header.down} icon={faChevronDown} />
+//           </Link>
+//           <ul className={header.hidden}>
+//             <li>
+//               <Link className={header.dropDownLink} to="/report">
+//                 Weekly Report
+//               </Link>
+//             </li>
+//             <li>
+//               <Link className={header.dropDownLink} to="/dashboard">
+//                 Dashboard
+//               </Link>
+//             </li>
+//           </ul>
+//         </li>
+
+//         <li>
+//           <Link className={header.link} to="/donate">
+//             Blog
+//           </Link>
+//         </li>
+//         <li>
+//           <Link className={header.link} to="/donate">
+//             Donate
+//           </Link>
+//         </li>
+//       </ul>
+//     </div>
+//   </header>
+// )
+
+// Header.propTypes = {
+//   siteTitle: PropTypes.string,
+// }
+
+// Header.defaultProps = {
+//   siteTitle: ``,
+// }
+
+// export default Header
+
+import React from "react"
+import { Link } from "gatsby"
+import header from "./header.module.scss"
+import { slide as Menu } from "react-burger-menu"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons"
 
-const Header = () => (
-  <header>
-    <div className={header.navBar}>
-      <ul>
-        <li>
-          <Link className={header.linkFirst} to="/">
-            Wozz
-            <FontAwesomeIcon className={header.down} icon={faChevronDown} />
-          </Link>
-          <ul className={header.hidden}>
-            <li>
-              <Link className={header.dropDownLink} to="/report">
-                Weekly Report
-              </Link>
-            </li>
-            <li>
-              <Link className={header.dropDownLink} to="/dashboard">
-                Dashboard
-              </Link>
-            </li>
-          </ul>
-        </li>
+export default props => {
+  return (
+    <Menu>
+      <Link className={header.linkFirst} to="/">
+        Wozz
+        <FontAwesomeIcon className={header.down} icon={faChevronDown} />
+      </Link>
+      <Link className={header.linkTwo} to="/">
+        Weekly Report
+      </Link>
+      <Link className={header.linkTwo} to="/dashboard">
+        Dashboard
+      </Link>
 
-        <li>
-          <Link className={header.link} to="/donate">
-            Blog
-          </Link>
-        </li>
-        <li>
-          <Link className={header.link} to="/donate">
-            Donate
-          </Link>
-        </li>
-      </ul>
-    </div>
-  </header>
-)
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
+      <Link className={header.link} to="/blog">
+        Blog
+      </Link>
+      <Link className={header.link} to="/donate">
+        Donate
+      </Link>
+    </Menu>
+  )
 }
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
-
-export default Header
