07c939c1cab1db2bf165d4ab9044bdfae266e740
Jon McEwen <>
2019-07-22 22:22:07 -0500

    cwm

--- src/pages/notes.js
+++ src/pages/notes.js
@@ -1,27 +1,39 @@
-import React from "react"
+import React, { Component } from "react"
 import noteStyles from "../components/noteStyles.module.scss"
 import Layout from "../components/layout"
 import Moment from "react-moment"
 
-const Notes = ({ location }) => {
-  const noteTiles = location.state.notes.map((e, i) => (
-    <div className={noteStyles.card}>
-      <Moment className={noteStyles.timeStamp} format="D MMM YYYY" withTitle>
-        {e.time}
-      </Moment>
+class Notes extends Component {
+  constructor() {
+    super()
+    this.state = {
+      notes: null,
+    }
+  }
+  componentWillMount() {
+    this.setState({ notes: this.props.location.state.notes })
+  }
+  render() {
+    const noteTiles = this.state.notes.map((e, i) => (
+      <div className={noteStyles.card}>
+        <Moment className={noteStyles.timeStamp} format="D MMM YYYY" withTitle>
+          {e.time}
+        </Moment>
 
-      <p className={noteStyles.contentStamp}>{e.content}</p>
-    </div>
-  ))
-  return (
-    <Layout>
-      <div className="container">
-        <div className="content">
-          <div className={noteStyles.box}>{noteTiles}</div>
-        </div>
+        <p className={noteStyles.contentStamp}>{e.content}</p>
       </div>
-    </Layout>
-  )
+    ))
+
+    return (
+      <Layout>
+        <div className="container">
+          <div className="content">
+            <div className={noteStyles.box}>{noteTiles}</div>
+          </div>
+        </div>
+      </Layout>
+    )
+  }
 }
 
 export default Notes
